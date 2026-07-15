41ee6f4a0439fb866e13afdce26b1eee6a1d1832
Valentina Leyva <yeissyleyva00@gmail.com>
2026-07-15 18:36:51 -0500

    creacion del archivo .env

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 //RESPONSABILIDAD DEL INDEX.JS: levantar el servidor y la conexion con la base de datos.
-const {server} = require("./src/server.js")
 const {pool} = require("./src/config/dbConnect.js")
+const {server} = require("./src/server.js")
+const { loadEnvFile } = require("node:process") //este modulo es para cargar las variables de entorno desde el archivo .env
+loadEnvFile('.env') //esto es para cargar las variables de entorno desde el archivo .env 
 
 
 const startServer = async () => { // es una funcion asincrona porque la conexion a la base de datos es asincrona
     await pool.query('SELECT 1') // esto es para probar la conexion a la base de datos, si no hay error es porque la conexion es correcta
     console.log("conexion a la base de datos establecida correctamente")
     
-    server.listen(3000, () =>{ // esto es para levantar el servidor en el puerto 3000
-        console.log("server listen on port 3000");
+    server.listen(process.env.PORT, () =>{ // esto es para levantar el servidor en el puerto 3000
+        console.log(`server listen on port: ${process.env.PORT}`);
     })
 }
 
 startServer()
 
-process.on("SIGINT", async () => {
+process.on("SIGINT", async () => { // con esto se cierra la conexion a la base de datos cuando se cierra el servidor
     await pool.end()
+    process.exit(0)
+})
+
+process.on("SIGTERM", async () => { // con esto se cierra la conexion a la base de datos cuando se cierra el servidor pero en la nube
+    await pool.end()
+    process.exit(0)
 })
--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,16 +1,19 @@
-const { pool } = require("pg")
+const { Pool } = require("pg")
+const { loadEnvFile } = require("node:process")
+loadEnvFile(".env")
 
-const pool = new pool({
-    host: 'localhost', //esto es la direccion de la base de datos, en este caso es local
-    port: 5432, // esto es el puerto de la base de datos, en este caso es el puerto por defecto de postgres
-    database: 'ft76M2', // esto es el nombre de la base de datos que se va a usar
-    user: 'postgres', // esto es el usuario de la base de datos, en este caso es el usuario por defecto de postgres
-    password: 12345, // esto es la contraseña del usuario de la base de datos, en este caso es la contraseña por defecto de postgres
-    max: 20, // esto es el numero maximo de conexiones que se pueden tener a la base de datos, en este caso es 20
-    idleTimeoutMillis: 30000, // esto es el tiempo que una conexion puede estar inactiva antes de ser cerrada, en este caso es 30 segundos
-    connectionTimeoutMillis: 2000 // esto es el tiempo que una conexion puede estar esperando para ser establecida antes de ser cerrada, en este caso es 2 segundos
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    max: process.env.DB_MAX_CONNECT,
+    idleTimeoutMillis: process.env.DB_IDLETIMEOUT,
+    connectionTimeoutMillis: process.env.DB_CONNECTIONTIMEOUT
 })
 
+    
 module.exports = {
     pool
 }
